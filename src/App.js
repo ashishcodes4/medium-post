5154828367adbb10df6452e828780d86069e511d
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-27 07:53:08 +0530

    state created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 import 'tachyons';
+import Clarifai from 'clarifai';
 import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+
+const app = new Clarifai.App({
+  apiKey: 'c1c6229976194ac49cb5820c17e3f6a6'
+ });
 
 
 const partilcesOptions = {
@@ -33,11 +38,18 @@ class App extends Component {
     }
   }
   onInputChange =  (event) => {
-    console.log(event.target.value)
+    console.log(event.target.value);
   }
 
   OnButtonSubmit = () => {
-    console.log('click')
+    app.models.predict("a403429f2ddf4b49b307e318f00e528b", "https://samples.clarifai.com/face-det.jpg").then(
+    function(response) {
+      console.log('inside the click event.')
+    },
+    function(err) {
+      // there was an error
+    }
+  );
   }
 
   render() {
