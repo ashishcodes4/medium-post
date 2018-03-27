09670bf36396a56d94f16c339f432015451a76e0
Ashish Nandan Singh <ashishcodes4@gmail.com>
2018-03-27 07:10:03 +0530

    adding particles

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react';
 import 'tachyons';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation'
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
 
+const partilcesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles className='particles'
+          params={partilcesOptions}
+        />
         <Navigation />
         <Rank />
         <ImageLinkForm />
